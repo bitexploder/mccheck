91fda25b6761fe2f24aedb02b31a0ca1bbf9a8fb
Jeremy Allen <jeremy.allen@carvesystems.com>
2014-01-28 15:32:29 -0500

    Updated to use the JSON API

--- mccheck.py
+++ mccheck.py
@@ -1,107 +1,80 @@
- #!/usr/bin/python
-
 import requests
-import HTMLParser
+import json
 import datetime
 import argparse
 
 def ts():
-	return str(datetime.datetime.now())
+    return str(datetime.datetime.now())
 
-class ParseMiddleCoin(HTMLParser.HTMLParser):
-	addr_data_keys = {6:"id",
-				 5:"accepted_hr",
-				 4:"rejected_hr",
-				 3:"imm_unexch_bal",
-				 2:"unexch_bal",
-				 1:"bal",
-				 0:"ignore"}
+class ParseMiddleCoin(object):
+    def __init__(self, json_bytes, lookfor):
+        self.save_tags = 0
+        self.addr_data = {}
+        self.addr = {}
+        self.lookfor = lookfor
+        self.json_bytes = json_bytes
 
-	def __init__(self, lookfor):
-        # initialize the base class
-		HTMLParser.HTMLParser.__init__(self)
-		self.save_tags = 0
-		self.addr_data = {}
-		self.addr = {}
-		self.lookfor = lookfor
+        self.json_bytes = json.loads(self.json_bytes)
 
-	def handle_starttag(self, tag, attrs):
-		if tag == "td":
-			for attr in attrs:
-				if(attr[0] == 'id'):
-					if attr[1] in lookfor:
-						self.addr_data = {}
-						self.save_tags = 6
+    def process_reports(self):
+        for address in self.lookfor:
+            for report in parser.json_bytes['report']:
+                if address == report[0]:
+                    self.addr[address] = {}
+                    report_data = report[1]
 
-	def handle_data(self, local_data):
-			if self.save_tags > 0:
-				addr_data_key = ParseMiddleCoin.addr_data_keys[self.save_tags]
-				self.addr_data[addr_data_key] = local_data
+                    if 'unexchangedBalance' in report_data:
+                        self.addr[address]['unexch_bal'] = \
+                            float(report_data['unexchangedBalance'])
+                    else:
+                        self.addr[address]['unexch_bal'] = 0.0
 
-	def handle_endtag(self, tag):
-		if self.save_tags > 0:
-			self.save_tags -= 1
+                    if 'bitcoinBalance' in report_data:
+                        self.addr[address]['bal'] = \
+                            float(report_data['bitcoinBalance'])
+                    else:
+                        self.addr[address]['bal'] = 0.0
 
-			if self.save_tags == 0:
-				for key in self.addr_data:
-					if self.addr_data[key] == '\n':
-						self.addr_data[key] = None				
+                    if 'immatureBalance' in report_data:                   
+                        self.addr[address]['imm_unexch_bal'] = \
+                            float(report_data['immatureBalance'])
+                    else:
+                        self.addr[address]['imm_unexch_bal'] = 0.0
 
-				if self.addr_data["imm_unexch_bal"] is not None:
-					self.addr_data["imm_unexch_bal"] = \
-						float(self.addr_data["imm_unexch_bal"])
-				else:
-					self.addr_data["imm_unexch_bal"] = 0
-
-				if self.addr_data["unexch_bal"] is not None:
-					self.addr_data["unexch_bal"] = \
-						float(self.addr_data["unexch_bal"])
-				else:
-					self.addr_data["unexch_bal"] = 0
-
-				if self.addr_data["bal"] is not None:
-					self.addr_data["bal"] = \
-						float(self.addr_data["bal"])
-				else:
-					self.addr_data["bal"] = 0
-
-				self.addr_data["btc_total"] = self.addr_data["bal"] + \
-											self.addr_data["unexch_bal"] + \
-											self.addr_data["imm_unexch_bal"]
-
-				addr_id = self.addr_data["id"]
-				self.addr[addr_id] = self.addr_data
+                    self.addr[address]['btc_total'] = \
+                        self.addr[address]['unexch_bal'] + \
+                        self.addr[address]['bal'] + \
+                        self.addr[address]['imm_unexch_bal']
 
 if __name__ == "__main__":
-	argparser = argparse.ArgumentParser(
-									description='Check on middlecoin stats')
+    argparser = argparse.ArgumentParser(description='Check on middlecoin stats')
+    argparser.add_argument('addresses', metavar='"address,address,address"', type=str, nargs='+',
+                       help='addresses to look for')
 
-	argparser.add_argument('addresses', metavar='"address,address,address"', 
-		type=str, nargs='+',help='addresses to look for')
+    args = argparser.parse_args()
+    lookfor = {}
+    for address in args.addresses[0].split(','):
+        lookfor[address.strip()] = True
 
-	args = argparser.parse_args()
-	lookfor = {}
-	for address in args.addresses[0].split(','):
-		lookfor[address.strip()] = True
+    print "[+] [%s] Requesting middlecoin data" % (ts())
+    r = requests.get('http://middlecoin.com/json')
+    text = r.text
 
-	print "[+] [%s] Requesting middlecoin data" % (ts())
-	r = requests.get('http://middlecoin.com/allusers.html')
-	text = r.text
-	print "[+] [%s] Got middlecoin data: %d bytes" % (ts(), len(text))
+    print "[+] [%s] Got middlecoin data: %d bytes" % (ts(), len(text))
+    print "[+] [%s] Parsing Text" % (ts())
 
-	print "[+] [%s] Parsing Text" % (ts())
-	parser = ParseMiddleCoin(lookfor)
-	parser.feed(text)
+    parser = ParseMiddleCoin(text, lookfor)
+    parser.process_reports()
 
-	print "[+] [%s] Printing CSV" % (ts())
+    print "[+] [%s] Printing CSV" % (ts())
+    print "Datetime,Address,BTC Total"
+    
+    btc_totals = []
+    for address in parser.addr:
+        print "%s, %s,%s, [%s]" % (ts(), address, 
+                                    parser.addr[address]["btc_total"], 
+                                    parser.addr[address])
+        
+        btc_totals.append(parser.addr[address]["btc_total"])
 
-	print "Datetime,Address,BTC Total"
-	btc_totals = []
-	for address in parser.addr:
-		print "%s, %s,%s, [%s]" % (ts(), address, 
-									parser.addr[address]["btc_total"], 
-									parser.addr[address])
-		
-		btc_totals.append(parser.addr[address]["btc_total"])
-
-	print "[+] [%s] Bitcoin Total: %s" % (ts(), sum(btc_totals))
+    print "[+] [%s] Bitcoin Total: %s" % (ts(), sum(btc_totals))
